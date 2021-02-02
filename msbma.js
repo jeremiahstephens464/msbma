ec86a310caab1f73018674df7958a2cab49b6d95
jeremiahstephens464 <78432747+jeremiahstephens464@users.noreply.github.com>
2021-02-02 17:54:47 +0200

    Add files via upload

---
+++ msbma.js
@@ -0,0 +1,18 @@
+function login(){
+	var value = document.getElementById('email1').value;
+}
+//document.getElementById("register").onClick = function() {
+	//document.getElementById("firstName").style.backgroundColor = "#000000";
+//};
+try {
+	document.getElementById('back').addEventListener('mouseout', function() {
+	document.getElementById('back').innerHTML = "<img src=\"images/back.png\"/>";
+	});
+}
+catch (e) {}
+try {
+	document.getElementById('back').addEventListener('mouseover', function() {
+	document.getElementById('back').innerHTML = "<img src=\"images/back1.png\"/>";
+	});
+}
+catch (e) {}
